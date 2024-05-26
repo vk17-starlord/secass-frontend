72e213067ab010b1114355f009a5334872f3f345
kunalbarve02 <kunalbarve02@gmail.com>
2024-05-26 21:58:37 +0530

    fix: alerts added fpr invite actions

--- src/services/InviteServiceWrapper.ts
+++ src/services/InviteServiceWrapper.ts
@@ -5,8 +5,10 @@ export class InviteService {
     try {
       const response = await axios.post('http://localhost:3002/api/v1/invites', payload);
       console.log('Invite created successfully:', response.data);
+      alert('Invite created successfully');
       return response.data;
     } catch (error) {
+      alert('Error creating Invite');
       console.error('Error creating Invite:', error);
       throw error;
     }
@@ -28,6 +30,7 @@ static async acceptInvite(inviteId: any) {
   try {
     const response = await axios.post(`http://localhost:3002/api/v1/invites/accept/${inviteId}`);
     console.log('Invite accepted:', response.data);
+    alert('Invite accepted successfully');
     return response.data
   } catch (error) {
     console.error('Error accepting invite:', error);
@@ -39,6 +42,7 @@ static async deleteInvite(inviteId: any) {
   try {
     const response = await axios.delete(`http://localhost:3002/api/v1/invites/${inviteId}`);
     console.log('Invite deleted:', response.data);
+    alert('Invite deleted successfully');
     return response.data
   } catch (error) {
     console.error('Error deleting invite:', error);
